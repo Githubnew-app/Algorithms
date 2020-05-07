b2c6f6ac6c1638d6882afb8876a8ef33ea37e9b7
MrVitaly <vitaly.email@mail.ru>
2020-05-07 20:02:01 +0300

    Add the solution for the task 'Winning tickets'

---
+++ Alghoritms.Solutions/Solutions/002_Tickets/TicketsSolution.cs
@@ -0,0 +1,41 @@
+﻿using Alghoritms.Solutions.Common;
+using System;
+using System.Numerics;
+
+namespace Alghoritms.Solutions
+{
+    [SolutionDescription("1.Tickets")]
+    public class TicketsSolution : ISolution
+    {
+        public string[] Run(string[] input) => new[] { FildTickets(Int32.Parse(input[0])).ToString() };
+
+        public BigInteger FildTickets(int numberOfDigits)
+        {
+            long maxSize = 9 * numberOfDigits;
+            var values = new long[maxSize + 1];
+            for (int i = 0; i <= 9; i++) values[i] = 1;
+            for (int n = 1; n < numberOfDigits; n++)
+            {
+                long localSum = 0;
+                long nextSize = 9 * (n + 1);
+                long size = nextSize / 2;
+                long lastValue = 0;
+                for (int i = 0; i <= size; i++)
+                {
+                    localSum += values[i] - lastValue;
+                    values[i] = localSum;
+                    lastValue = values[nextSize - i];
+                    values[nextSize - i] = localSum;
+                }
+            }
+
+            BigInteger sum = 0;
+            for (int i = 0; i <= maxSize; i++)
+            {
+                BigInteger value = (BigInteger)values[i];
+                sum += value * value;
+            }
+            return sum;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ ___
 
   - To create a project for checking homework tasks. Each task is presented as a set of files with a description of the task ("**problem.txt**"), set of test cases ("**test.*N*.in**", where ***N*** is the number of the test case), and an appropriate set of answers ("**test.*N*.out**"). A solution for a task can be added to the project as a separate class(es). It should be able to add new tasks and appropriate solutions to the project. The 'test environment' should allow checking a solution and show whether test cases passed or not. 
   - To check the proper functioning of the 'test environment' via sample of task '**0.String**'.
+
+- Winning tickets
+  - To find amount of winning ticket among N*2 digits numbers.
